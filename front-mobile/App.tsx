fb9765643a7efdfd61171ee91b04a682c714fc8a
Edmar <edmarpmc@gmail.com>
2021-12-17 15:20:38 -0300

    Add Header

--- front-mobile/App.tsx
+++ front-mobile/App.tsx
@@ -1,11 +1,25 @@
+import { useFonts, OpenSans_700Bold, OpenSans_400Regular } from '@expo-google-fonts/open-sans';
+import AppLoading from 'expo-app-loading';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Header from './src/core/components/Header';
+import Login from './src/Pages/Login';
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    OpenSans_700Bold, 
+    OpenSans_400Regular
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  } 
+  
   return (
     <View style={styles.container}>
-      <Text>Ola Mobile!</Text>
+     <Header />
+     <Login />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,9 +27,8 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  flex: 1,
+  backgroundColor: '#525252',
+    
   },
 });
---
+++ front-mobile/src/Pages/Movies/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+ function Login() {
+  return (
+    <View >
+        <Text>Catalogo de filmes</Text>
+    </View>
+     
+   
+  );
+}
+export default Login;
